27e146a77fde5a21ce23ce6db7137b9a372038d6
raeli1 <raeli1@LPI-CND40804H3>
2017-01-27 13:55:11 +0100

    aa

--- UserManegment/UserManegment/Controllers/TestController.cs
+++ UserManegment/UserManegment/Controllers/TestController.cs
@@ -9,12 +9,8 @@ namespace UserManegment.Controllers
 {
     public class TestController : ApiController
     {
-     //razzouk brunch
-        //public IHttpActionResult GetUser()
-        //{
-
-        //    return Ok(new User { Address = "sfsd", Id = 1, FirstName = "razzouk", LastName = "hhh", City = "khgajhg", Phone = "234234", PassWord = "asdasd" ,ORG = { new ORG { Name = "cybercom" }, new ORG { Name = "sabb" } } });
-        //}
+     
+     
 
     }
 }
