b05c4fa5d4b1573bf9ffb94535d8f91841ea37d9
raeli1 <raeli1@LPI-CND40804H3>
2017-01-27 15:53:59 +0100

    changeDataBaseTables2

--- UserManegment/UserManegment/Models/UserInOrg.cs
+++ UserManegment/UserManegment/Models/UserInOrg.cs
@@ -7,13 +7,17 @@ namespace UserManegment.Models
 {
     public class UserInOrg
     {
+        public UserInOrg() {
+            LogInRegistry = new HashSet<LogInRegistry>();
+        }
         public int Id { get; set; }
         public int UserId { get; set; }
 
         public int OrgId { get; set; }
         public int RoleId { get; set; }
-       
 
+        public virtual WorkTitel WorkTitel { get; set; }
         public virtual Role Role { get; set; }
+        public virtual ICollection<LogInRegistry> LogInRegistry { get; set; }
     }
 }
--- UserManegment/UserManegment/Models/WorkTitel.cs
+++ UserManegment/UserManegment/Models/WorkTitel.cs
@@ -10,15 +10,18 @@ namespace UserManegment.Models
     public partial class WorkTitel
     {
         public int Id { get; set; }
-
+        public WorkTitel()
+        {
+            UserInOrg = new HashSet<UserInOrg>();
+        }
         [Required]
         [StringLength(50)]
         public string Name { get; set; }
 
         public int UserInOrgId { get; set; }
 
-    
+        public virtual ICollection<UserInOrg> UserInOrg { get; set; }
 
-       
+
     }
 }
