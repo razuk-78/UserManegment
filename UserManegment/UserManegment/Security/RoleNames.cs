b52d0a624efbe32d39073c7b5b8998e6f8c6e4b5
raeli1 <raeli1@LPI-CND40804H3>
2017-02-02 09:09:15 +0100

    aaa

---
+++ UserManegment/UserManegment/Security/RoleNames.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace UserManegment.Security
+{
+  public class RoleNames
+    {
+        public static string Create { get { return "create"; } private set { } }
+        public static string Read
+        {
+            get { return "Read"; }
+            private set { }
+        }
+        public static string Write { get { return "write"; } private set { }  }
+    }
+}
